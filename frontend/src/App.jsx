17cd1d15bd66f306f5589567e6427bf2936610b1
Henoker <hennybany@gmail.com>
2024-01-05 20:06:53 +0300

    feat: pages setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'; // Importing necessary components from 'react-router-dom' for routing.
+// import Home from './view/shop/Home';
+// import StoreHeader from './view/base/StoreHeader';
+// import StoreFooter from './view/base/StoreFooter';
+// import Cart from './view/shop/Cart';
+// import Checkout from './view/shop/Checkout';
+// import PaymentSuccess from './view/shop/PaymentSuccess';
+// import ProductDetail from './view/shop/ProductDetail';
+// import Register from './view/auth/Register';
+import Login from './view/auth/Login';
+// import CreatePassword from './view/auth/CreatePassword';
+// import ForgotPassword from './view/auth/ForgotPassword';
+// import Logout from './view/auth/Logout';
+// import Shipping from './view/shop/Shipping';
+// import Invoice from './view/shop/Invoice';
+// import Account from './view/customer/Account';
+// import Orders from './view/customer/Orders';
+// import OrderDetail from './view/customer/OrderDetail';
+// import Notifications from './view/customer/Notifications';
+// import Settings from './view/customer/Settings';
+// import Wishlist from './view/customer/Wishlist';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        {/* <StoreHeader /> */}
+        <Routes>
+          {/* Authentication Routes */}
+
+          {/* <Route path="/register/" element={<Register />} /> */}
+          <Route path="/login/" element={<Login />} />
+          {/* <Route path="/logout/" element={<Logout />} />
+          <Route path="/create-password/" element={<CreatePassword />} />
+          <Route path="/forgot-password/" element={<ForgotPassword />} /> */}
+
+          {/* Store Routes */}
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/detail/:slug/" element={<ProductDetail />} />
+          <Route path="/cart/" element={<Cart />} />
+          <Route path="/shipping/" element={<Shipping />} />
+          <Route path="/checkout/" element={<Checkout />} />
+          <Route path="/success/" element={<PaymentSuccess />} />
+          <Route path="/invoice/:order_oid/" element={<Invoice />} /> */}
+
+          {/* Buyer Routes */}
+          {/* <Route path="/customer/account/" element={<Account />} />
+          <Route path="/customer/orders/" element={<Orders />} />
+          <Route path="/customer/order/detail/:order_oid/" element={<OrderDetail />} />
+          <Route path="/customer/wishlist/" element={<Wishlist />} />
+          <Route path="/customer/notifications/" element={<Notifications />} />
+          <Route path="/customer/settings/" element={<Settings />} /> */}
+
+        </Routes>
+        {/* <StoreFooter /> */}
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/view/customer/OrderDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+function OrderDetail() {
+    return (
+        <div>OrderDetail</div>
+    )
+}
+
+export default OrderDetail
